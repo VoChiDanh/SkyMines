4ce73750bc45de2b9695d148604c4c15d50e0578
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-09-03 01:57:45 +0700

    fix gradle, upgrade to 1.3.0-SNAPSHOT

--- src/main/java/com/github/colingrime/updater/SpigotUpdater.java
+++ src/main/java/com/github/colingrime/updater/SpigotUpdater.java
@@ -10,24 +10,33 @@ import java.net.URL;
 
 public class SpigotUpdater {
 
-	private static final String UPDATE_URL = "https://api.spigotmc.org/legacy/update.php?resource=101373";
-	private static final String RESOURCE_URL = "https://www.spigotmc.org/resources/skymines.101373/";
+    private static final String UPDATE_URL = "https://api.spigotmc.org/legacy/update.php?resource=101373";
+    private static final String RESOURCE_URL = "https://www.spigotmc.org/resources/101373/";
 
-	private final SkyMines plugin;
+    private final SkyMines plugin;
 
-	public SpigotUpdater(SkyMines plugin) {
-		this.plugin = plugin;
-	}
+    public SpigotUpdater(SkyMines plugin) {
+        this.plugin = plugin;
+    }
 
-	public void checkForUpdate() {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(UPDATE_URL).openStream()))) {
-			String latestVersion = reader.readLine();
-			if (!plugin.getDescription().getVersion().equals(latestVersion)) {
-				Logger.warn("A new update has been found (v" + latestVersion + ")");
-				Logger.warn("Download it here: " + RESOURCE_URL);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public void checkForUpdate() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(UPDATE_URL).openStream()))) {
+            String latestVersion = reader.readLine();
+            String pluginVersion = plugin.getDescription().getVersion();
+            String latestNumber = latestVersion.replace(".", "");
+            String pluginNumber = pluginVersion.replace(".", "");
+            if (!pluginVersion.contains("-SNAPSHOT")) {
+                if (Integer.parseInt(latestNumber) > Integer.parseInt(pluginNumber)) {
+                    Logger.warn("A new update has been found (v" + latestVersion + ")");
+                    Logger.warn("Download it here: " + RESOURCE_URL);
+                }
+            } else {
+                Logger.warn("You are running dev build version, be careful!");
+                Logger.warn("Your version: " + pluginVersion + " (#" + pluginNumber.replace("-SNAPSHOT", "") + ")");
+                Logger.warn("Latest version: " + latestVersion + " (#" + latestNumber + ")");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

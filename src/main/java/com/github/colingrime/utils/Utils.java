150538ef9c49c81ab34923f481059ac2295921e8
VoChiDanh <vochidanh.hs.c@gmail.com>
2023-08-14 18:04:00 +0700

    change to pom

--- src/main/java/com/github/colingrime/utils/Utils.java
+++ src/main/java/com/github/colingrime/utils/Utils.java
@@ -2,7 +2,7 @@ package com.github.colingrime.utils;
 
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.apache.commons.lang.WordUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
